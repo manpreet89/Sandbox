474731254300063b509f4e840d48ea094ec52195
JC443852 <manpreet757@yahoo.in>
2016-12-09 20:01:03 +1000

    its python

--- oddname.py
+++ oddname.py
@@ -3,3 +3,12 @@ __author__ = 'JC443852'
     manpreet
 
 """
+valid = False
+while not valid:
+ name = input(" enter your name:")
+ if name != "" and not name.isspace():
+    valid = True
+
+l = len(name)
+for i in range(0,l,2):
+    print(name[i])
